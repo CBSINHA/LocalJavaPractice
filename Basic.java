9218f776778dac3551f2148268200895fe993727
Shashank Sinha <shashanksinha590@gmail.com>
2024-12-23 22:26:27 +0530

    just learning java

---
+++ Basic.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Basic{
+    public static void main(String args[]){
+        Scanner output=new Scanner(System.in);
+        System.out.println("Enter your name:\n");
+        String name=output.nextLine();
+        System.out.println("Your name is: "+name);
+    }
+
+}
