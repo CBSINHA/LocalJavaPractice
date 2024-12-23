8ca282fb841295d3ed84b999ae5bbafbccdf973b
Shashank Sinha <shashanksinha590@gmail.com>
2024-12-23 22:50:26 +0530

    updated GUI using java(JOptionPane)

--- Gui.java
+++ Gui.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 public class Gui {
     public static void main(String[] arg) {
+        System.out.println("You are about to see a simple GUI based off Java");
         JOptionPane.showMessageDialog(null,"Welcome to simple java GUI");
         String name=JOptionPane.showInputDialog("What's your name?");
         JOptionPane.showMessageDialog(null, "Your name is "+name);
