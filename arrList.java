aaf7b63d7b383f1974fa8c727c2bef6faed60820
Shashank Sinha <shashanksinha590@gmail.com>
2024-12-28 22:20:10 +0530

    ArrayList

---
+++ arrList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arrList {
+    public static void main(String[] args) {
+        ArrayList<Integer> age=new ArrayList<Integer>();
+        age.add(1);
+        age.add(2);
+        age.add(3);
+        age.add(4);
+        age.add(5);
+        for(int i:age){
+            System.out.print(i+" ");
+        }
+
+    }
+}
